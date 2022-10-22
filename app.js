3c8c166cf64013bc3bbd9d4b62f587da4189db0e
Fiorella2003 <fiorequispe0@gmail.com>
2022-10-22 09:14:22 -0300

    Segunda entrega

--- app.js
+++ app.js
@@ -1,48 +1,87 @@
-class Usuario {
-  constructor(nombre, apellido) {
-    //Atributos
-    this.nombre = nombre;
-    this.apellido = apellido;
-    this.libros = [];
-    this.mascotas = [];
+const fs = require("fs");
+
+class Contenedor {
+  constructor(nombreArchivo) {
+    this.nombreArch = nombreArchivo;
   }
-  //Metodos
-  //Retornar todo el nombre
-  getFullName() {
-    return this.nombre + "" + this.apellido;
+  async save(object) {
+    let array = [];
+    try {
+      const data = await fs.promises.readFile(this.nombreArch, "utf-8");
+      array = JSON.parse(data);
+      //Encontrar object segun id
+      let idArray = array.map((obj) => obj.id);
+      let maxId = Math.max(...idArray);
+
+      object.id = maxId + 1;
+      array.push(object);
+      fs.writeFileSync(this.nombreArch, JSON.stringify(array));
+
+      console.log(`El archivo tiene como maximo de productos: ${object.id}`);
+    } catch {
+      object.id = 0;
+      array.push(object);
+      fs.writeFileSync(this.nombreArch, JSON.stringify(array));
+    }
+    return object.id;
   }
-  //Agregar un nuevo animal al array
-  addMascota(nombreAnimal) {
-    this.mascotas.push(nombreAnimal);
+  async getById(id) {
+    try {
+      const data = await fs.promises.readFile(this.nombreArch, "utf-8");
+      let array = JSON.parse(data);
+      const producto = array.find((obj) => obj.id === id);
+      if (producto) {
+        return producto;
+      } else {
+        console.log("No se encontró producto");
+        return null;
+      }
+    } catch {
+      console.log("No existe el id");
+    }
   }
-  //Retorna numero animales
-  countMascotas() {
-    return this.mascotas.length;
+  async getAll() {
+    try {
+      const data = await fs.promises.readFile(this.naombreArch, "utf-8");
+      const array = JSON.parse(data);
+      return array;
+    } catch {
+      return null;
+    }
   }
-  //Agregar dato de un libro al array de libros
-  addBook(nombre, autor) {
-    //Agregar objeto
-    const dataLibro = { nombre: nombre, autor: autor };
-    this.libros.push(dataLibro);
+  async deleteById(number) {
+    try {
+      const data = await fs.promises.readFile(this.nombreArch, "utf-8");
+      const array = JSON.parse(data);
+      const nuevoArray = array.filter((obj) => obj.id !== number);
+      fs.writeFileSync(this.nombreArch, JSON.stringify(nuevoArray));
+    } catch {
+      return console.log("No hay objetos en el archivo");
+    }
   }
-  //Retorna un array con nombre de libros
-  getBookNames() {
-    //Busca los nombres del array de libros
-    return this.libros.map((libro) => libro.nombre);
+  async deleteAll() {
+    try {
+			let data = await fs.promises.readFile(this.nombreArch, "utf8");
+			let dataArch = JSON.parse(data);
+			if (dataArch.length) {
+				await fs.promises.writeFile(this.nombreArch, "");
+			} else {
+				console.log("No hay productos que borrar");
+			}
+		} catch {
+			console.log("Error");
+		}
   }
 }
 
-//Crear objeto usuario
-const usuario = new Usuario ("Fiorella", "Yao")
-//Mostrar todo el nombre del usuario
-console.log(usuario.getFullName())
-//Agregar mascotas nuevas al usuario
-usuario.addMascota('Marley')
-usuario.addMascota('Guardian')
-usuario.addMascota('Fiona')
-usuario.addMascota('Doky')
-console.log(usuario.countMascotas())
-//Agregar libros al usuario
-usuario.addBook('Harry Potter','J.K.Rowling')
-usuario.addBook('Cincuenta sombras de Grey','E.L.James')
-console.log(usuario.getBookNames())
+const contenedor = new Contenedor("./productos.txt");
+//Agregar un producto más
+contenedor.save({title: "Alimento Sobrecito Balanceado 7",price: 3040,thumbnail: "pedigree.png",}).then((resolve) => console.log(resolve));
+//Filtramos por id
+contenedor.getById(1).then((resolve) => console.log("El producto buscado es: ", resolve));
+//Todos los productos
+contenedor.getAll().then((resolve) =>console.log("Estos son todos los productos existentes:", resolve));
+//Eliminar producto segun id
+contenedor.deleteById(3)
+//Eliminar todos los productos
+contenedor.deleteAll
